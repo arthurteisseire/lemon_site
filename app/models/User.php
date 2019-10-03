279761cd747be821edeabff13abf627053dbc82d
arthur <arthur.teisseire@epitech.eu>
2019-10-03 18:29:16 +0200

    Implement create function in User

--- app/models/User.php
+++ app/models/User.php
@@ -1,13 +1,30 @@
 <?php
 
-require_once 'config.php';
-
 class User
 {
     public $name;
 
-    public function create()
+    public function create($array)
     {
-        $conn->query("insert...");
+        DataBaseSingleTon::getInstance()->query(
+            'INSERT INTO users (name, firstname, birthday, mail, sexe, country, job) VALUES (' . $this->arrayToValues($array) . ')');
+    }
+
+    private function arrayToValues($array)
+    {
+        return $this->paramToValues($array['name'], $array['firstname'], $array['birthday'], $array['mail'],
+            $array['sexe'], $array['country'], $array['job']);
+    }
+
+    private function paramToValues(...$params)
+    {
+        $values = "";
+        $comma = "";
+        foreach ($params as $param) {
+            $values .= $comma;
+            $values .= $param;
+            $comma = ",";
+        }
+        return $values;
     }
 }
