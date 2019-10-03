79538cecf7eb6677b6490ae96bfdc5181acc0540
arthur <arthur.teisseire@epitech.eu>
2019-10-03 20:09:48 +0200

    Improve visual quality of register form

--- app/views/index.php
+++ app/views/index.php
@@ -1,13 +1,30 @@
-<form method="post" action="../Register">
-    <label> Nom : <input type="text" name="name"/> </label>
-    <label> Prénom <input type="text" name="firstname"/> </label>
-    <label> Date de Naissance : <input type="date" name="birthday"/> </label>
-    <label> Mail : <input type="email" name="mail"/> </label>
+<form method="post" action="../Register" class="col mx-4 my-4">
+    <div class="form-group row">
+        <label> Nom <input type="text" name="name" class="form-control"/> </label>
+    </div>
+    <div class="form-group row">
+        <label> Prénom <input type="text" name="firstname" class="form-control"/> </label>
+    </div>
+    <div class="form-group row">
+        <label> Date de Naissance <input type="date" name="birthday" class="form-control"/> </label>
+    </div>
+    <div class="form-group row">
+        <label> Mail <input type="email" name="mail" class="form-control"/> </label>
+    </div>
 
-    Sexe : <label> <input type="radio" name="sexe" value="male"/> Masculin </label>
-    <label> <input type="radio" name="sexe" value="female"/> Féminin </label>
+    <div class="form-group row">
+        Sexe
+    </div>
+        <div class="form-group row">
+            <label class="mr-4"> <input type="radio" name="sexe" value="male"/> Masculin </label>
+            <label> <input type="radio" name="sexe" value="female"/> Féminin </label>
+        </div>
 
-    <label> Pays : <input type="text" name="country"/> </label>
-    <label> Métier : <input type="text" name="job"/> </label>
+    <div class="form-group row">
+        <label> Pays <input type="text" name="country" class="form-control"/> </label>
+    </div>
+    <div class="form-group row">
+        <label> Métier <input type="text" name="job" class="form-control"/> </label>
+    </div>
     <input type="submit"/>
 </form>
