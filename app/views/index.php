b269d129d1f2c4b9605555ff146680cc99c0bedb
arthur <arthur.teisseire@epitech.eu>
2019-10-03 19:45:28 +0200

    Add Bootstrap to project

--- app/views/index.php
+++ app/views/index.php
@@ -1,16 +1,13 @@
-<html>
-<body>
-
 <form method="post" action="../Register">
     <label> Nom : <input type="text" name="name"/> </label>
     <label> Prénom <input type="text" name="firstname"/> </label>
     <label> Date de Naissance : <input type="date" name="birthday"/> </label>
     <label> Mail : <input type="email" name="mail"/> </label>
-    <label> Sexe : <input type="text" name="sexe"/> </label>
+
+    Sexe : <label> <input type="radio" name="sexe" value="male"/> Masculin </label>
+    <label> <input type="radio" name="sexe" value="female"/> Féminin </label>
+
     <label> Pays : <input type="text" name="country"/> </label>
     <label> Métier : <input type="text" name="job"/> </label>
     <input type="submit"/>
 </form>
-
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
+<html lang="fr">
+<head>
+    <title>Lemon site</title>
+    <link rel="stylesheet" href="vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+</head>
+<body>
+
 <?php
 
 require_once 'app/core/init.php';
 
 $app = new App();
+
+?>
+
+</body>
+<script src="vendor/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+</html>
