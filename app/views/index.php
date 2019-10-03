f7031155251ea653df2e4d7ad4805887112abceb
arthur <arthur.teisseire@epitech.eu>
2019-10-03 20:11:38 +0200

    Format html style

--- app/views/index.php
+++ app/views/index.php
@@ -15,10 +15,10 @@
     <div class="form-group row">
         Sexe
     </div>
-        <div class="form-group row">
-            <label class="mr-4"> <input type="radio" name="sexe" value="male"/> Masculin </label>
-            <label> <input type="radio" name="sexe" value="female"/> Féminin </label>
-        </div>
+    <div class="form-group row">
+        <label class="mr-4"> <input type="radio" name="sexe" value="male"/> Masculin </label>
+        <label> <input type="radio" name="sexe" value="female"/> Féminin </label>
+    </div>
 
     <div class="form-group row">
         <label> Pays <input type="text" name="country" class="form-control"/> </label>
