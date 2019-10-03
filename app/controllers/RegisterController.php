3ed2767491a8b1d2f259441d9ebaac02193633f6
arthur <arthur.teisseire@epitech.eu>
2019-10-03 12:45:10 +0200

    Add model to Controller

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -2,8 +2,10 @@
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function index($name = '')
     {
-        echo 'home/index';
+        $user = $this->model('User');
+        $user->name = $name;
+        echo $user->name;
     }
 }
