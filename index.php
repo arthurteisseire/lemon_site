b53271dd6fc18472c094845e8782e2bf08c99c00
arthur <arthur.teisseire@epitech.eu>
2019-10-05 15:39:18 +0200

    Move css outside app

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <title>Lemon site</title>
     <link rel="stylesheet" href="/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/app/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
