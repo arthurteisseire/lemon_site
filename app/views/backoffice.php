53d135671a61a1399431b14d24ae8c272ba76d22
arthur <arthur.teisseire@epitech.eu>
2019-10-05 20:27:55 +0200

    Refactor backoffice

--- app/views/backoffice.php
+++ app/views/backoffice.php
@@ -1,30 +1,25 @@
 <div class="container-fluid">
-
     <?php
     $countriesGroup = $data['countriesGroup'];
     foreach ($countriesGroup as $country => $users) {
-        echo '<div>' . $country . "</div>";
+        echo '<div class="col-xl">' . $country . '</div>';
         printInfoRow($country);
         foreach ($users as $user)
             printUserRow($user);
         echo '<br><br>';
     }
     ?>
-
 </div>
 
 <?php
 function printUserRow($user)
 {
     echo '<div class="row">';
-    echo '<div class="col-sm">
-            <a href="/BackOffice/edit/' . $user['id'] . '">edit</a>
-            <a href="/BackOffice/delete/' . $user['id'] . '">delete</a>
-          </div>';
+    printCol('<a href="/BackOffice/edit/' . $user['id'] . '">edit</a>
+                    <a href="/BackOffice/delete/' . $user['id'] . '">delete</a>');
     $keys = ['name', 'firstname', 'birthday', 'mail', 'sexe', 'country', 'job'];
-    array_map(function ($key) use ($user) {
-        echo '<div class="col-sm">' . $user[$key] . '</div>';
-    }, $keys);
+    foreach ($keys as $key)
+        printCol($user[$key]);
     echo '</div>';
 }
 
@@ -32,14 +27,12 @@ function printInfoRow($countryName)
 {
     $keys = ['NAME', 'FIRSTNAME', 'BIRTHDAY', 'MAIL', 'SEXE', 'COUNTRY', 'JOB'];
     echo '<div class="row">';
-    printInfoCol('<a href="/BackOffice/add/' . $countryName . '">add</a>');
-    array_map('printInfoCol', $keys);
+    printCol('<a href="/BackOffice/add/' . $countryName . '">add</a>');
+    array_map('printCol', $keys);
     echo '</div>';
 }
 
-function printInfoCol($data)
+function printCol($data)
 {
-    echo '<div class="col-sm" style="background-color:rgba(0,0,0,0.5)">' . $data . '</div>';
+    echo '<div class="col-sm">' . $data . '</div>';
 }
-
-?>
