b40904c9f4238318e7fabef58e943b6004015dd6
arthur <arthur.teisseire@epitech.eu>
2019-10-05 09:45:40 +0200

    Check if user is admin to let him get access to backoffice

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -5,8 +5,12 @@ class AdminController extends Controller
     public function index()
     {
         $error = [];
-        if (isset($_GET['error']) && $_GET['error'] == 1)
-            $error = ['error' => 'Invalid form'];
+        if (isset($_GET['error'])) {
+            if ($_GET['error'] == 1)
+                $error = ['error' => 'Invalid form'];
+            else if ($_GET['error'] == 2)
+                $error = ['error' => 'User is not admin'];
+        }
         $this->view('admin', $error);
     }
 }
--- app/controllers/BackOfficeController.php
+++ app/controllers/BackOfficeController.php
@@ -4,10 +4,26 @@ class BackOfficeController extends Controller
 {
     public function index()
     {
+        $this->checkPost();
+        $this->redirectIfAdmin();
+    }
+
+    public function checkPost()
+    {
         if (empty($_POST['mail']) || empty($_POST['password'])) {
             header("Location: /Admin?error=1");
             exit();
         }
-        $this->view('backoffice');
+    }
+
+    public function redirectIfAdmin()
+    {
+        $user = $this->model('User');
+        if ($user->isAdmin($_POST['mail'], $_POST['password']))
+            $this->view('backoffice');
+        else {
+            header("Location: /Admin?error=2");
+            exit();
+        }
     }
 }
